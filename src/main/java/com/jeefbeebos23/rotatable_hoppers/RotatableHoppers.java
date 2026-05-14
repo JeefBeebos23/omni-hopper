e2d2fbf4052973b812558990f7cabac2578748ea
Aidan Guinn <wbguinn77@gmail.com>
2026-05-13 22:39:23 -0700

    feat: add project skeleton and entry point

---
+++ src/main/java/com/jeefbeebos23/rotatable_hoppers/RotatableHoppers.java
@@ -0,0 +1,10 @@
+package com.jeefbeebos23.rotatable_hoppers;
+
+import net.fabricmc.api.ModInitializer;
+
+public class RotatableHoppers implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        // All behavior is in mixins; nothing to register here.
+    }
+}
